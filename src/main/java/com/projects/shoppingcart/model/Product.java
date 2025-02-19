d4419c8799a83557ad9296bc583a524671bc53c4
abdulmalik <abdelmaleka011@gmail.com>
2025-02-19 20:55:37 +0200

    Add initial files, category, product, and image class

---
+++ src/main/java/com/projects/shoppingcart/model/Product.java
@@ -0,0 +1,32 @@
+package com.projects.shoppingcart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String brand;
+    private int inventory;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)//if a Category is saved/updated/deleted, it affects the Product, but not the other way around.
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+}
